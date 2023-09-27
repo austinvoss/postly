9925248a40eb686d526b4d2f0ea75132043b4121
Voss <vossaustin@gmail.com>
2023-09-26 19:00:02 -0700

    implemented react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 export default function App() {
   return (
-    <div class="container mx-auto text-neutral-50">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </div>
+    <Router>
+      <div className="container mx-auto text-neutral-50">
+        <Switch>
+          <Route path="/posts">{/* My Posts Component Here */}</Route>
+          <Route path="/profile">{/* My Profile Component Here */}</Route>
+          <Route path="/login">{/* My Login Component Here */}</Route>
+          <Route path="/register">{/* My Register Component Here */}</Route>
+          <Route path="/">{/* My Home Component or Redirect Here */}</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
